5bc5b91b9ad24c45fa33dfb6866e84578164ec0d
Art Hill <art@hill.org>
2021-08-25 21:14:59 +0000

    framework complete

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('README.rst', encoding='UTF-8') as f:
+    readme = f.read()
+setup(
+    name='pgbackup',
+    version='0.1.0',
+    description='Database backups locally or to AWS S3.',
+    long_description=readme,
+    author='Art Hill',
+    author_email='art@hill.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[]
+)
+
